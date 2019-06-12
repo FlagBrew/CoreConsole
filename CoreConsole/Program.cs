6c4cb1bdb437aa64e982de668cf39f0317ccd394
Allen <allenlydiard@gmail.com>
2019-06-11 23:53:41 -0300

    Added -server option

--- CoreConsole/Program.cs
+++ CoreConsole/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 using CoreConsole.APIs;
 using PKHeX.Core;
 
@@ -13,33 +17,43 @@ namespace CoreConsole
         {
             string appPath = Environment.CurrentDirectory;
 
-            Initialize(args);
-            if (args.Contains("-l"))
+         
+           if (args.Contains("-server"))
             {
-                // Legality API calls
-                var lc = new LegalityCheck(pk);
-                if (args.Contains("--verbose")) Console.WriteLine(lc.VerboseReport);
-                else Console.WriteLine(lc.Report);
+                var server = new Server();
+                server.Server_start();
             }
-            if (args.Contains("-alm"))
+            else
             {
-                if (!args.Contains("--version")) Console.WriteLine("Specify version with the [--version] tag");
-                else
+                Initialize(args);
+                if (args.Contains("-l"))
                 {
-                    var alm = new AutoLegality(pk, args[Array.IndexOf(args, "--version") + 1]);
-                    if (alm != null)
-                    {   
-                        if (!args.Contains("-o"))
+                    // Legality API calls
+                    var lc = new LegalityCheck(pk);
+                    if (args.Contains("--verbose")) Console.WriteLine(lc.VerboseReport);
+                    else Console.WriteLine(lc.Report);
+                }
+                if (args.Contains("-alm"))
+                {
+                    if (!args.Contains("--version")) Console.WriteLine("Specify version with the [--version] tag");
+                    else
+                    {
+                        var alm = new AutoLegality(pk, args[Array.IndexOf(args, "--version") + 1]);
+                        if (alm != null)
                         {
-                            string output = Util.CleanFileName(alm.GetLegalPKM().FileName);
-                            File.WriteAllBytes(Path.Combine(appPath, "output", output), alm.GetLegalPKM().DecryptedBoxData);
-                        } else
-                        {
-                            string output = GetOutputPath(args);
-                            File.WriteAllBytes(output, alm.GetLegalPKM().DecryptedBoxData);   
+                            if (!args.Contains("-o"))
+                            {
+                                string output = Util.CleanFileName(alm.GetLegalPKM().FileName);
+                                File.WriteAllBytes(Path.Combine(appPath, "output", output), alm.GetLegalPKM().DecryptedBoxData);
+                            }
+                            else
+                            {
+                                string output = GetOutputPath(args);
+                                File.WriteAllBytes(output, alm.GetLegalPKM().DecryptedBoxData);
+                            }
                         }
+                        else Console.WriteLine("Invalid version");
                     }
-                    else Console.WriteLine("Invalid version");
                 }
             }
         }
@@ -55,8 +69,13 @@ namespace CoreConsole
             }
             else
             {
+                if(Array.IndexOf(args, "--set") == -1)
+                {
+                    Console.WriteLine("Missing arguments!");
+                    Environment.Exit(1);
+                }
                 string set = args[Array.IndexOf(args, "--set") + 1];
-                bool valid = Enum.TryParse<GameVersion>(args[Array.IndexOf(args, "--version") + 1], true, out var game);
+                _ = Enum.TryParse<GameVersion>(args[Array.IndexOf(args, "--version") + 1], true, out var game);
                 var template = PKMConverter.GetBlank(game.GetGeneration(), game);
                 template.ApplySetDetails(new ShowdownSet(set.Split(new string[] { "\\n" }, StringSplitOptions.None)));
                 pk = template;
@@ -71,6 +90,62 @@ namespace CoreConsole
         private static string GetOutputPath(string[] args)
         {
             return args[Array.IndexOf(args, "-o") + 1];
+        }
+    }
+}
+
+public class Server
+{
+    readonly TcpListener server = new TcpListener(IPAddress.Loopback, 7272);
+    public void Server_start()
+    {
+        server.Start();
+        Accept_connection();
+        Console.WriteLine("Started Server!");
+        while (true)
+        {
+            Thread.Sleep(5000);
+        }
+    }
+
+
+    private void Accept_connection()
+    {
+        server.BeginAcceptTcpClient(Handle_connection, server);
+    }
+
+    public void Handle_connection(IAsyncResult result)
+    {
+        Accept_connection();
+        TcpClient client = server.EndAcceptTcpClient(result);
+
+        NetworkStream ns = client.GetStream();
+
+        while (client.Connected)
+        {
+            byte[] size = new byte[8];
+            ns.Read(size, 0, size.Length);
+            string dataSizeStr = Encoding.UTF8.GetString(size, 0, size.Length);
+
+            int.TryParse(dataSizeStr, out int dataSize);
+
+            byte[] msg = new byte[dataSize]; 
+            try
+            {
+                ns.Read(msg, 0, msg.Length)
+                var pk = PKMConverter.GetPKMfromBytes(msg);
+                var lc = new LegalityCheck(pk);
+                byte[] report = Encoding.Default.GetBytes(lc.Report);
+                ns.Write(report, 0, report.Length);
+                ns.Flush();
+                client.Close();
+            } catch
+            {
+                byte[] err = Encoding.Default.GetBytes("Not a Pokemon!");
+                ns.Write(err, 0, err.Length);
+                ns.Flush();
+                client.Close();
+            }
         }
     }
 }
